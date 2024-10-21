9e00f55a93f57a50e66bc2506c7a8fba9b98a80d
Szymon Kacprzak <skacprzak@s201.local>
2024-10-21 08:30:03 +0200

    usunąłem komentarze aby poprawić czytelność kodu

--- calculator.consoleapp/Program.cs
+++ calculator.consoleapp/Program.cs
@@ -4,28 +4,26 @@
     {
         static void Main(string[] args)
         {
-            //1. Wyświetlenie nagłowka - opis aplikacji
+            
             Console.WriteLine("Witaj w aplikacji KALKULATOR!");
 
-            //2. Prośba o podanie 1 liczby
+        
             Console.WriteLine("Podaj proszę 1 liczbę:");
 
-            //3. Pobranie liczby od użytkownika
+            
             var number1 = int.Parse(Console.ReadLine());
 
-            //4. Prośba o podanie działania
+           
             Console.WriteLine("Jaką operację chcesz wykonać? Możliwe operacje to: '+', '-', '*', '/'.");
 
-            //5. Pobranie wybranego działania od użytkownika
             var operation = Console.ReadLine();
 
-            //6. Prośba o podanie 2 liczby
+      
             Console.WriteLine("Podaj proszę 2 liczbę:");
 
-            //7. Pobranie liczby od użytkownika
+          
             var number2 = int.Parse(Console.ReadLine());
 
-            //8. Wykonanie obliczeń
             var result = 0;
 
             switch (operation)
@@ -46,12 +44,12 @@
                     throw new Exception("Wybrałeś złą operację!");
             }
             
-            //9. Wyświetlenie wyniku użytkownikowi
+           
             Console.WriteLine($"Wynik Twojego działania to: {result}.");
         }
         catch (Exception ex)
             {
-                //logowanie do pliku
+               
                 Console.WriteLine(ex.Message);
             }
 }
